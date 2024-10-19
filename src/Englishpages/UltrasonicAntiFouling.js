c5171dd7ce39322fca7488064596a288922a8c00
Deniz <136066030+ThecodinggirlDenise@users.noreply.github.com>
2024-10-20 00:37:13 +0300

    uLTRASONIC WAVES

--- src/Englishpages/UltrasonicAntiFouling.js
+++ src/Englishpages/UltrasonicAntiFouling.js
@@ -1,5 +1,9 @@
+/* eslint-disable no-undef */
+/* eslint-disable react/jsx-no-undef */
 /* eslint-disable jsx-a11y/alt-text */
 import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faArrowRightLong } from "@fortawesome/free-solid-svg-icons";
 // Ensure the path is correct
 
 export default function UltrasonicAntiFouling() {
@@ -20,6 +24,7 @@ export default function UltrasonicAntiFouling() {
           </div>
         </div>
       </section>
+
       <div
         className=" w-24  max-w-5xl  h-24 bg-cover bg-center bg-no-repeat rounded-[33px] -mt-20 z-30 relative mx-auto "
         style={{
@@ -27,6 +32,132 @@ export default function UltrasonicAntiFouling() {
           backgroundSize: "cover",
         }}
       ></div>
+      <img src={"/images/Blog-1.jpg"} className="w-[30rem] ml-96 mt-12"></img>
+      <h1 className="text-4xl ml-96  mt-10 font-semibold w-72">
+        What is an Ultrasonic Anti-FoulingSystem?
+      </h1>
+      <div className="sticky top-[90px] cta-sidebar w-full lg:w-80 flex-shrink-0 bg-slate-800 p-6 rounded-lg shadow-md mt-10 lg:mt-0 lg:h-80 ml-auto">
+        <h2 className="text-2xl lg:text-4xl font-bold mb-4 text-white font-montserrat text-center lg:text-left">
+          Get a Special Pricing for Your Vessels!
+        </h2>
+        <button className="bg-white text-black  rounded-lg py-2 px-5 w-full lg:w-64 font-semibold mt-4 hover:bg-blue-800 transition">
+          Get a Quote
+          <FontAwesomeIcon icon={faArrowRightLong} className="ml-2" />
+        </button>
+      </div>
+
+      <div className="text-sm">
+        <p className="pl-96 mr-[70rem] mt-12 ">
+          Accumulation of unwanted marine organisms on a boat’s hull can give a
+          rough ride to boatowners and affect their lifetime. Boat owners
+          frequently struggle with fouling,which results in catastrophic
+          scenarios on the marine vessel, dragging all thefun and pleasure of
+          marine activities with it. Fouling not only affects the funtime but
+          also causes major efficiency and maneuverability problems, raising
+          safetyconcerns. Althoughtraditional anti-fouling techniques such as
+          using biocide chemicals,anti-fouling bottom paints, or toxic
+          anti-fouling coatings might mitigate theeffects of these organisms,
+          adopting a technological approach opens doors tocost-effective and
+          ecologically friendly solutions.
+        </p>
+        <p className="pl-96 mr-[70rem] mt-12 ">
+          Marisonia hasdeveloped a revolutionary machine technology designed to
+          solve fouling problemsin an environmentally friendly way: an
+          ultrasonic anti-fouling system. Pioneeredby Marisonia, the
+          ultrasonicanti-fouling system is an innovative approach for thebattle
+          against fouling on the boats. With ultrasonic boat
+          antifouling,Marisonia offers comprehensive solutions with minimal
+          maintenance, extendedlifespan, fuel and cost efficiency, and
+          eco-friendly options against biofouling.
+        </p>
+        <p className="pl-96 mr-[70rem] mt-12 ">
+          In its broadsense, ultrasonic anti-fouling refers to using ultrasonic
+          waves to preventfouling. Ultrasonic waves are emitted into the water
+          surrounding the marinevessel. These high-frequency waves prevent the
+          settlement and adhesion ofharmful fouling organisms such as algae,
+          plants, and parasitic animals to theboat hull and other surfaces,
+          eliminating a habitable environment. This methodis particularly
+          efficient due to its reduced environmental impact andcost-friendly
+          attributes.
+        </p>
+        <p className="pl-96 mr-[70rem] mt-12 mb-9">
+          With itscutting-edge ultrasonic anti-fouling systems, Marisonia offers
+          continuousprotection against unwelcome guests. This non-toxic and
+          eco-friendly approachnot only ensures precision in cleaning and
+          protection but also contributes toincreased fuel efficiency, reduced
+          maintenance efforts, and a prolongedlifespan for marine structures.
+          Marisonia'sUltrasonic Anti-Fouling stands out as a novel and
+          eco-friendly method thatallows boat owners to prevent fouling in a way
+          that is both cutting-edge andecologically responsible.
+        </p>
+        <p className="pl-96 mr-[70rem] mt-12 mb-9">
+          Ultrasonic anti-foulingsystems are designed to utilize these
+          high-frequency waves with specificimplementations and devices,
+          achieving comprehensive and fully automatedsolutions to fouling
+          prevention. The system consists of transducers andultrasonic emitters
+          that transfer ultrasonic waves into the water.
+        </p>
+        <p className="pl-96 mr-[70rem] mt-12 mb-9">
+          These systemsoperate continuously or at intervals, depending on the
+          design and choice of theboat owner. Having a user-friendly interface,
+          ultrasonic anti-fouling systemsrequire minimum maintenance and
+          intervention. With these systems, Marisoniaprovides a second option
+          for biofouling solutions. Ultrasonic anti-foulingsystem is a
+          well-acknowledged method for surface cleaning and boat maintenance.The
+          seamless integration of these systems assures ever-lasting enjoyment
+          forthe boat owners by preventing fouling without significant effort.
+          Once set up,these systems will operate smoothly without requiring
+          maintenance for a longtime.
+        </p>
+        <p className="pl-96 mr-[70rem] mt-12 mb-9">
+          These systemsoperate continuously or at intervals, depending on the
+          design and choice of theboat owner. Having a user-friendly interface,
+          ultrasonic anti-fouling systemsrequire minimum maintenance and
+          intervention. With these systems, Marisoniaprovides a second option
+          for biofouling solutions. Ultrasonic anti-foulingsystem is a
+          well-acknowledged method for surface cleaning and boat maintenance.The
+          seamless integration of these systems assures ever-lasting enjoyment
+          forthe boat owners by preventing fouling without significant effort.
+          Once set up,these systems will operate smoothly without requiring
+          maintenance for a longtime.
+        </p>
+        <p className="pl-96 mr-[70rem] mt-12 mb-9">
+          Marisonia’s ultrasonicanti-fouling for boats also has a non-toxic
+          nature. Due to the gradual wear-offof the antifouling properties over
+          time, regular maintenance and reapplicationof antifouling bottom paint
+          are necessary. This doubles the trouble due tosustainability issues.
+          Seeking to safeguard boats and marine vessels, Marisoniaalso revolves
+          around the safety of maritime. Unlike traditional
+          chemical-basedanti-fouling methods, ultrasonic anti-fouling systems do
+          not rely on harmfulsubstances, making them environmentally
+          sustainable. So, how does the anti-foulingsystem work, and does
+          ultrasonic anti-fouling really work? ‍
+        </p>
+        <p className="pl-96 mr-28 mt-12 mb-9">
+          How Does an UltrasonicAnti-Fouling System Work?
+        </p>
+        <p className="pl-96 mr-28 mt-12 mb-9">
+          Ultrasonic anti-fouling system utilizes ultrasonic emitters on the
+          boat’s surface,typically on the boat’s hull and running gears. These
+          devices generatehigh-frequency waves that are beyond the range of
+          human hearing. After the useractivates these devices, the ultrasonic
+          waves propagate through water,providing a safe energy field around the
+          surface to be protected. Thesehigh-frequency waves play an important
+          role, especially because they interferewith the natural frequency
+          intervals of the fouling organisms as they rely onspecific ranges to
+          communicate, multiply, and navigate through the sea andsurfaces they
+          contact. This makes it challenging for organisms to attach to
+          theboat's hull or other protected surfaces. Even if some organisms
+          manage toattach, the adverse conditions created by the ultrasonic
+          waves prevent theirgrowth and multiplication. The systememits
+          ultrasonic waves without any turbulence or noise, which allows
+          thedevices to operate silently without disturbing both human and
+          marine life. Thissilent operation ensures that boat owners can enjoy a
+          peaceful and undisturbedboating experience. With proven efficiency and
+          credibility, ultrasonic anti-refoulingsystems are becoming an
+          essential part of marine vessel maintenance day by day. ‍
+        </p>
+      </div>
     </div>
   );
 }
